d3cd93f8517ad690962aea3796a1a8292ec0a4c1
athul-peter <95914468+athul-peter@users.noreply.github.com>
2021-12-10 16:35:53 +0530

    Add files via upload

---
+++ Aptitude Test.py
@@ -0,0 +1,113 @@
+#Aptitude Test Code
+#This Code is written and Compiled by Athul Peter
+#This is written on the 9th of December 2021
+#This is free to edit and make further improvements under the Creative Commons Licence
+#Enjoy and find out your True Passion.
+from time import sleep
+sleep(0.1)
+print("Hi")
+sleep(0.1)
+print('Hello')
+sleep(0.1)
+print('Much Appreciations to your decision to check a suitable career pathway ')
+sleep(0.1)
+print('Without further delay lets start')
+sleep(0.1)
+print('Press Enter to Begin the Aptitude Test')
+input()
+sleep(0.2)
+print("Please tell me your name:")
+name = input()
+sleep(0.2)
+print("Okay Thankyou," + name + " Good to see you here, now tell me your Age please(in numbers)")
+age = int(input())
+print("Okay now one more thing, What kind of Personality type do you believe you are?")
+sleep(0.2)
+print('The following are the sixtien personality types popularly identified by psychologists.')
+sleep(1)
+print("1.Architect : They are highly imaginative and strategic thinkers, with a plan for everything")
+sleep(1)
+print('2. Logician : They are innovative inventors with an unquenchable thirst for knowledge')
+sleep(1)
+print('3.Commander: They are Bold, imaginative and strong-willed leaders, always finding a way or making one.')
+sleep(1)
+print('4.Debator: Smart adn curious thinkers who cannot resist an intellectual challenge')
+sleep(1)
+print('5.Advocate: Quiet and mystical, yet very inspiring and tireless idealist.')
+sleep(1)
+print('6.Mediator: Poetic, kind and altruistic people, always eager to help a good cause.')
+sleep(1)
+print('7.Protagonist: Charismatic and inspiring leaders, able to mesmerize their listeners.')
+sleep(1)
+print('8.Campaigner: Enthusiastic, creative and sociable free spirits, who can always find a reason to smile.')
+sleep(1)
+print('9.Logistician : Practical and fact minded individuals, whose reliability cannot be doubted')
+sleep(1)
+print('10.Defender: Very dedicated and warm protectors, always ready to defend their loved ones.')
+sleep(1)
+print('11.Executive : Excellent administrators, unsurpassed at managing things or people')
+sleep(1)
+print('12.Consul: Extraordinary caring, social and popular people, always eager to help.')
+sleep(1)
+print('13. Virtuoso : bold, and practical experimenters, masters of al kinds of tools')
+sleep(1)
+print('14.Adventurer: Flexible adn charming artists, always ready to explore and experience something new')
+sleep(1)
+print('15.Entrepneur : Smart, energetic and very perceptive people, who truly enjoy living on the edge.')
+sleep(1)
+print('16.Entertainer : Spontaneous, energetiic and enthusiastic people-life is never boring around them')
+sleep(2)
+print('Okey dokey! now you have got 16 Seconds to go through all 16 personality types, is that fast? I know you can do it soon.')
+sleep(16)
+print('Hope you enjoyed reading it!')
+sleep(0.1)
+print('Now back to the question, Just enter the personality type number following (eg. Type 1 for Architect and so on and press enter ')
+print('Mmmm... Wonder, What to write there? Write what your gut tells you!'+ name)
+career = int(input())
+sleep(1)
+print('Wonderful!'+ name)
+sleep(2)
+print('I have one thing to tell you')
+sleep(5)
+if career == 1:
+    print("You will be a Great Architect one day")
+elif career == 2:
+    print("You will be a Great Logician one day")
+elif career == 3:
+    print('You will be a Great Commander One Day')
+elif career == 4:
+    print("You will be a Great Debator One Day")
+elif career == 5:
+    print('You will be a Great Advocate One Day')
+elif career == 6:
+    print('You will be a Great Mediator One day')
+elif career == 7:
+    print('You will be a Great Protagonist one day')
+elif career == 8:
+    print('You will be a Great Campaigner One day')
+elif career == 9:
+    print('You will be a Great Logistician One day')
+elif career == 10:
+    print('You will be a Great Defender One day')
+elif career == 11:
+    print('You will be a Great Executive One day')
+elif career == 12:
+    print('You will be a Great Consul One day')
+elif career == 13:
+    print('You will be a Great Virtuoso One Day')
+elif career == 14:
+    print('You will be a Great Adventurer one day')
+elif career == 15:
+    print('You will be a Great Entrepreneur one day')
+elif career == 16:
+    print('You will be a Great Entertainer One Day')
+sleep(2)
+print('Hope you are happy with the most valuable discovery you have made')
+sleep(0.1)
+print('Yes, Self Discovery')
+sleep(0.1)
+print('Keeping that in mind! study and work hard for your dream')
+print('Good bye and Good luck')
+input()
+exit()
+
